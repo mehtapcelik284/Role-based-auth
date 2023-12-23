3bb99a76205533e1076a2b81c4586fb2c3a73d3c
Mehtap Celik <mehtapcelik284@gmail.com>
2023-12-23 20:48:32 +0300

    created project && added jwt && some auth methods

---
+++ src/auth/guard/role.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from '../model/role.enum';
+import { ROLES_KEY } from '../decorator';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    ['admin', 'premium'];
+
+    return requiredRoles.some((role) => user.role?.includes(role));
+  }
+}
