0546c19eb57a48a260f412515f2b7238e2a65612
Mehtap Celik <mehtap.celik@hologramsciences.com>
2024-01-06 16:41:25 +0300

    role usage has been added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,8 +2,12 @@ import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/modules/auth/decorator/get-user.decorator';
 import { JwtGuard } from 'src/modules/auth/guard';
+import { Role } from '../auth/model/role.enum';
+import { RoleGuard } from '../auth/guard/role.guard';
+import { Roles } from '../auth/decorator';
 
-@UseGuards(JwtGuard)
+@Roles(Role.DOCTOR)
+@UseGuards(JwtGuard, RoleGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
